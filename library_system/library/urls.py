f1ff1e0e13ce7bf7caa1122cdc90efa29a4c1bd4
Ronakp08 <Ronakp6393@gmail.com>
2025-03-06 17:14:51 +0530

    updated

--- library_system/library/urls.py
+++ library_system/library/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, BookViewSet, BoorowedBookViewSet
+from .views import UserViewSet, BookViewSet, BorrowedBookViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'books', BookViewSet)
-router.register(r'borrowed-books', BoorowedBookViewSet)
+router.register(r'borrowed-books', BorrowedBookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('borrowed-books/borrowed_by/', BorrowedBookViewSet.as_view({'get': 'borrowed_by'}), name='borrowed_by'),
+    path('borrowed-books/return_date/', BorrowedBookViewSet.as_view({'get': 'return_date'}), name='return_date'),
 ]
--- library_system/library/views.py
+++ library_system/library/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import User, Book, BorrowedBook
 from .serializers import UserSerializer, BookSerializer, BorrowedBookSerializer
 
@@ -13,6 +15,31 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
-class BoorowedBookViewSet(viewsets.ModelViewSet):
+class BorrowedBookViewSet(viewsets.ModelViewSet):
     queryset = BorrowedBook.objects.all()
     serializer_class = BorrowedBookSerializer
+
+    # API to get books borrowed by a specific user
+    @action(detail=False, methods=['get'])
+    def borrowed_by(self, request):
+        user_id = request.query_params.get('user_id')
+        book_id = request.query_params.get('book_id')
+
+        if user_id:
+            borrowed_books = BorrowedBook.objects.filter(UserID=user_id)
+        elif book_id:
+            borrowed_books = BorrowedBook.objects.filter(BookID=book_id)
+        else:
+            return Response([])
+
+        serializer = BorrowedBookSerializer(borrowed_books, many=True)
+        return Response(serializer.data)
+
+    # API to get the return date 
+    @action(detail=False, methods=['get'])
+    def return_date(self, request):
+        user_id = request.query_params.get('user_id')
+
+        borrowed_books = BorrowedBook.objects.filter(UserID=user_id)
+        return_dates = borrowed_books.values('BookID__Title', 'ReturnDate')
+        return Response(return_dates)
